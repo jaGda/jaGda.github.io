1574581ccf4978bf9d7304d0a76184ba4330d8b5
jagda <danielzychniewicz@wp.pl>
2021-09-19 08:00:21 +0200

    Adjusting endpoints to back-end layer

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
-  // const apiRoot = "http://localhost:8080/v1/task/";
-  // const trelloApiRoot = "http://localhost:8080/v1/trello/";
-  const apiRoot = "https://floating-coast-55211.herokuapp.com/v1/task/";
-  const trelloApiRoot = "https://floating-coast-55211.herokuapp.com/v1/trello/";
+  const apiRoot = "http://localhost:8080/v1/";
+  const trelloApiRoot = "http://localhost:8080/v1/trello/";
+  // const apiRoot = "https://floating-coast-55211.herokuapp.com/v1/task/";
+  // const trelloApiRoot = "https://floating-coast-55211.herokuapp.com/v1/trello/";
   const datatableRowTemplate = $("[data-datatable-row-template]").children()[0];
   const $tasksContainer = $("[data-tasks-container]");
 
@@ -14,7 +14,7 @@ $(document).ready(function () {
   getAllTasks();
 
   function getAllAvailableBoards(callback, callbackArgs) {
-    var requestUrl = trelloApiRoot + "getTrelloBoards";
+    var requestUrl = trelloApiRoot + "boards";
 
     $.ajax({
       url: requestUrl,
@@ -76,7 +76,7 @@ $(document).ready(function () {
   }
 
   function getAllTasks() {
-    const requestUrl = apiRoot + "getTasks";
+    const requestUrl = apiRoot + "tasks";
 
     $.ajax({
       url: requestUrl,
@@ -97,7 +97,7 @@ $(document).ready(function () {
     var taskId = parentEl.attr("data-task-id");
     var taskTitle = parentEl.find("[data-task-name-input]").val();
     var taskContent = parentEl.find("[data-task-content-input]").val();
-    var requestUrl = apiRoot + "updateTask";
+    var requestUrl = apiRoot + "tasks";
 
     $.ajax({
       url: requestUrl,
@@ -123,15 +123,10 @@ $(document).ready(function () {
   function handleTaskDeleteRequest() {
     var parentEl = $(this).parents("[data-task-id]");
     var taskId = parentEl.attr("data-task-id");
-    var requestUrl = apiRoot + "deleteTask";
+    var requestUrl = apiRoot + "tasks/";
 
     $.ajax({
-      url:
-        requestUrl +
-        "/?" +
-        $.param({
-          taskId: taskId,
-        }),
+      url: requestUrl + taskId,
       method: "DELETE",
       success: function () {
         parentEl.slideUp(400, function () {
@@ -147,7 +142,7 @@ $(document).ready(function () {
     var taskTitle = $(this).find('[name="title"]').val();
     var taskContent = $(this).find('[name="content"]').val();
 
-    var requestUrl = apiRoot + "createTask";
+    var requestUrl = apiRoot + "tasks";
 
     $.ajax({
       url: requestUrl,
@@ -192,7 +187,7 @@ $(document).ready(function () {
   }
 
   function handleCardCreationRequest(event) {
-    var requestUrl = trelloApiRoot + "createTrelloCard";
+    var requestUrl = trelloApiRoot + "cards";
     var $relatedTaskRow = $(event.target).parents("[data-task-id]");
     var relatedTaskId = $relatedTaskRow.attr("data-task-id");
     var relatedTask = availableTasks[relatedTaskId];
